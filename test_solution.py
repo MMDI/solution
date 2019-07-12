db8f596f88162612e693fa5d2271f73323cd2860
James Castleberry <thisoneisreal@gmail.com>
2019-07-12 06:24:16 -0500

    improved code quality
    - removed old unused code
    - added more docstrings
    - removed trailing whitespaces

--- test_solution.py
+++ test_solution.py
@@ -1,42 +1,39 @@
 #!/usr/bin/env python3
 
 import asyncio
+import collections
+import socket
 import aiohttp
 import aiohttp.test_utils
 import aiohttp.web
 import pytest
-import json
-import uuid
-import collections
-import inspect
-import socket
-import weakref
 import solution
-from typing import Optional, Dict
 
 _RedirectContext = collections.namedtuple('RedirectContext', 'add_server session')
 
 @pytest.fixture
 async def aiohttp_redirector():
+    """ Redirect requests to local test server """
     resolver = FakeResolver()
     connector = aiohttp.TCPConnector(resolver=resolver, use_dns_cache=False)
     async with aiohttp.ClientSession(connector=connector) as session:
         yield _RedirectContext(add_server=resolver.add, session=session)
 
 class CaseControlledTestServer(aiohttp.test_utils.RawTestServer):
+    """ Test server that relies on test case """
     def __init__(self, **kwargs):
         super().__init__(self._handle_request, **kwargs)
         self._requests = asyncio.Queue()
         self._responses = {}                # {id(request): Future}
 
     async def close(self):
-        ''' cancel all pending requests before closing '''
+        """ Cancel all pending requests before closing """
         for future in self._responses.values():
             future.cancel()
         await super().close()
 
     async def _handle_request(self, request):
-        ''' push request to test case and wait until it provides a response '''
+        """ Push request to test case and wait until it provides a response """
         self._responses[id(request)] = response = asyncio.Future()
         self._requests.put_nowait(request)
         try:
@@ -46,22 +43,25 @@ class CaseControlledTestServer(aiohttp.test_utils.RawTestServer):
             del self._responses[id(request)]
 
     async def receive_request(self):
-        ''' wait until test server receives a request '''
+        """ Wait until test server receives a request """
         return await self._requests.get()
 
     def send_response(self, request, *args, **kwargs):
-        ''' send web response from test case to client code '''
+        """ Send web response from test case to client code """
         response = aiohttp.web.Response(*args, **kwargs)
         self._responses[id(request)].set_result(response)
 
 class FakeResolver:
+    """ Resolves tested addresses to local test servers """
     def __init__(self):
         self._servers = {}
 
     def add(self, host, port, target_port):
+        """ Add a tested address """
         self._servers[host, port] = target_port
 
-    async def resolve(self, host, port=0, family=socket.AF_INET):
+    async def resolve(self, host, port=0):
+        """ Resolve a host:port into a connectable address """
         try:
             fake_port = self._servers[host, port]
         except KeyError:
@@ -70,7 +70,6 @@ class FakeResolver:
             'hostname': host,
             'host': '127.0.0.1',
             'port': fake_port,
-            'family': socket.AF_INET,
             'proto': 0,
             'flags': socket.AI_NUMERICHOST,
         }]
@@ -89,7 +88,7 @@ async def test_try_login(aiohttp_redirector):
                              text='{"access_token": "3da2cb47-489c-48f7-957e-6fbf5a75fc47"}',
                              content_type='application/json')
         token = await task
-        assert token ==  '3da2cb47-489c-48f7-957e-6fbf5a75fc47'
+        assert token == '3da2cb47-489c-48f7-957e-6fbf5a75fc47'
 
 @pytest.mark.asyncio
 async def test_try_get(aiohttp_redirector):
